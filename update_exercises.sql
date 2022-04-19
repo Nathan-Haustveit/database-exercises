c4405adc6aee9ead0b14cca714b0fda4795d0ad7
Nathan Haustveit <nathanhaustveit@gmail.com>
2022-04-19 09:27:48 -0500

    completed update exercise

---
+++ update_exercises.sql
@@ -0,0 +1,18 @@
+USE codeup_test_db;
+
+SELECT name as 'All albums in your table.' FROM albums;
+UPDATE albums
+SET sales = sales * 10;
+
+SELECT name as 'All albums released before 1980' FROM albums WHERE release_date < 1980;
+UPDATE albums
+SET release_date = 1800
+WHERE release_date < 1980;
+
+SELECT name as 'All albums by Michael Jackson' FROM albums where artist = 'Michael Jackson';
+UPDATE albums
+SET artist  = 'Peter Jackson'
+WHERE artist = 'Michael Jackson';
+
+
+
